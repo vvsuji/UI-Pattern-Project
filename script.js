6fecf30fa573a07779fb4bf3db509a169aea926c
Suji Falcon <suji.falcon@gmail.com>
2022-09-30 16:22:47 -0400

    mvp

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '69e3926b9amshc2e276640bb471ap143807jsn929aa6cf1ecb',
-    'X-RapidAPI-Host': 'league-of-legends-champions.p.rapidapi.com',
-  },
-};
+const url = `http://ddragon.leagueoflegends.com/cdn/12.6.1/data/en_US/champion.json`;
 
-fetch(
-  'https://league-of-legends-champions.p.rapidapi.com/champions/en-us?page=1&size=10',
-  options,
-)
+const container = document.querySelector('.container');
+
+fetch(url)
   .then((response) => response.json())
-  .then((response) => {
-    let res = response.champions;
-    iter(res);
+  .then((res) => {
+    const champions = Object.entries(res.data);
+    for (let i = 0; i < champions.length; i++) {
+      const img = document.createElement('img');
+      img.classList.add('cardImg');
+      img.src = `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champions[i][0]}_0.jpg`;
+
+      img.id = i;
+      img.addEventListener('click', (e) => {
+        let champName = document.querySelector('#champ-name').innerText;
+        champName = champions[e.target.id][0];
+        console.log(champName);
+        let champTitle = document.querySelector('#champ-title').innerText;
+        champTitle = champions[i][1].title;
+        console.log(champions[i][1].title);
+        let champDesc = document.querySelector('#champ-desc').innerText;
+        champDesc = champions[i][1].blurb;
+        let champSplash = document.querySelector('#champ-splash');
+        champSplash.src = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champions[i][0]}_0.jpg`;
+        document.querySelector('#modal').style.display = 'block';
+      });
+      document
+        .querySelector('#modal')
+        .addEventListener(
+          'click',
+          () => (document.querySelector('#modal').style.display = 'none'),
+        );
+
+      const card = document.createElement('article');
+      card.classList.add('card');
+      card.appendChild(img);
+      container.appendChild(card);
+    }
   })
   .catch((err) => console.error(err));
-
-function iter(arr) {
-  arr.forEach((item) => {
-    for (node in item) {
-      console.log(item[node]['champion_name']);
-      console.log(item[node]['champion_splash']);
-    }
-  });
-}
